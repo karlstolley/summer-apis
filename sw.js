554d9251aef8ffc451cfa167f1d780ced5658aac
Karl Stolley <karl.stolley@gmail.com>
2018-07-07 16:27:00 -0500

    Set up cache- and fetch-based responses

--- sw.js
+++ sw.js
@@ -58,3 +58,78 @@ addEventListener('activate', e => {
     })
   );
 });
+
+addEventListener('fetch', fe => {
+  const request = fe.request;
+  // Pages: Try the network first; cache a copy; serve offline page as a fallback
+  if (request.headers.get('Accept').includes('text/html')) {
+    fe.respondWith(
+      fetch(request)
+      .then(fetch_response => {
+        const copy = fetch_response.clone();
+        fe.waitUntil(
+          caches.open(cache_of.pages)
+          .then(this_cache => {
+            this_cache.put(request, copy);
+          })
+        );
+        return fetch_response;
+      })
+      .catch(error => {
+        return caches.match(request)
+        .then(cached_response => {
+          if(cached_response) {
+            return cached_response;
+          }
+          return caches.match(detected_scope+'offline.html');
+        });
+      })
+    );
+    return;
+  }
+  // External requests: Try the cache first; update cache from network
+  if (!request.url.includes(location.hostname)) {
+    fe.respondWith(
+      caches.match(request)
+      .then(cached_response => {
+        if(cached_response) {
+          fe.waitUntil(
+            fetch(request)
+            .then(fetch_response => {
+              caches.open(cache_of.requests)
+              .then(this_cache => {
+                return this_cache.put(request, fetch_response);
+              });
+            })
+          );
+          return cached_response;
+        }
+        return fetch(request)
+        .then(fetch_response => {
+          const copy = fetch_response.clone();
+          fe.waitUntil(
+            caches.open(cache_of.requests)
+            .then(this_cache => {
+              this_cache.put(request, copy);
+            })
+          );
+          return fetch_response;
+        });
+      })
+    );
+    return;
+  }
+  // Everything else: Try the cache first, then the network
+  fe.respondWith(
+    caches.match(request)
+    .then(cached_response => {
+      if(cached_response) {
+        return cached_response;
+      }
+      return fetch(request)
+      .catch(error => {
+        return caches.match(detected_scope+'offline.html');
+      });
+    })
+  );
+});
