8d77d759a1c4b71f4a50368f2e9396bfb392f089
Karl Stolley <karl.stolley@gmail.com>
2018-07-07 14:44:08 -0500

    Make use of object literals for clearer code

--- sw.js
+++ sw.js
@@ -1,29 +1,41 @@
-const version = '0.0.1'
-const static_cache = 'static_files-' + version;
-
+const version = '0.0.2'
 // Detect the service worker's location to set a scope for absolute paths
 const detected_scope = '/'+location.href.split('/')[3]+'/';
 
-// Simplified asset manifest, with call to .map() to prepend the absolute,
-// detected scope onto each asset listed
-const hosted_asset_manifest = [
-  'screen.css',
-  'site.js',
-  'offline.html'
-].map(asset => detected_scope + asset);
-const remote_asset_manifest = [
-  '//fast.fonts.net/cssapi/d645cd83-c955-4277-a114-11f6324025d4.css',
-  'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
-];
+const cache_of = {
+  assets: 'assets-' + version,
+  requests: 'requests',
+  pages: 'pages'
+};
 
-const cached_assets = hosted_asset_manifest.concat(remote_asset_manifest);
+const preloaded_manifests = {
+  essential: [
+    'screen.css',
+    'site.js',
+    'offline.html'
+  ].map(asset => detected_scope + asset),
+  supporting: [
+    '//fast.fonts.net/cssapi/d645cd83-c955-4277-a114-11f6324025d4.css',
+    'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
+  ]
+};
+
+const cache_list = [
+  cache_of.assets,
+  cache_of.requests,
+  cache_of.pages
+]
 
 addEventListener('install', e => {
   console.log('The service worker is waiting to install...');
+  console.log(preloaded_manifests.essential);
   e.waitUntil(
-    caches.open(static_cache)
-    .then(cache => {
-      return cache.addAll(cached_assets);
+    caches.open(cache_of.assets)
+    .then(this_cache => {
+      // Nice-to-have assets
+      this_cache.addAll(preloaded_manifests.supporting);
+      // Must-have assets
+      return this_cache.addAll(preloaded_manifests.essential);
     })
   );
 });
@@ -35,30 +47,14 @@ addEventListener('activate', e => {
     .then(cache_names => {
       return Promise.all(
         cache_names.map(cache_name => {
-          if (cache_name !== static_cache) {
+          if (!cache_list.includes(cache_name)) {
             return caches.delete(cache_name);
           }
         })
       );
     })
-    .then( () => {
+    .then(() => {
       return clients.claim();
-    })
-  );
-});
-
-addEventListener('fetch', e => {
-  const request = e.request;
-  e.respondWith(
-    caches.match(request)
-    .then(cachedResponse => {
-      if(cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(request)
-      .catch(error => {
-        return caches.match(detected_scope+'offline.html');
-      });
     })
   );
 });
